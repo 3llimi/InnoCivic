840b82f79ad7e5eea4d9f808e0099d4b661810f2
Gpshfrd <cl27f27f@gmil.com>
2025-11-03 17:58:17 +0300

    1.0.1

--- bro.config.js
+++ bro.config.js
@@ -2,7 +2,7 @@ export default {
   apiPath: 'stubs/api',
   webpackConfig: {
     output: {
-      publicPath: `/static/innocivic/0.0.1/`
+      publicPath: `/static/innocivic/1.0.1/`
     }
   },
   /* use https://admin.bro-js.ru/ to create config, navigations and features */
